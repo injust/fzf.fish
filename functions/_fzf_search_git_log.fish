45bc16b43be8757421151d547164a8aeff166f75
Justin Su <injustsu@gmail.com>
2025-10-29 14:11:44 -0400

    [fix] Use `string split --fields` instead of `--field`

--- functions/_fzf_search_git_log.fish
+++ functions/_fzf_search_git_log.fish
@@ -24,7 +24,7 @@ function _fzf_search_git_log --description="Search the output of git log and pre
         )
         if test $status -eq 0
             for line in $selected_log_lines
-                set -f abbreviated_commit_hash (string split --field=1 " " $line)
+                set -f abbreviated_commit_hash (string split --fields=1 " " $line)
                 set -f full_commit_hash (git rev-parse $abbreviated_commit_hash)
                 set -fa commit_hashes $full_commit_hash
             end
