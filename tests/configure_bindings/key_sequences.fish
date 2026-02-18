9313a75339c4915e99d2d6e0fc40edb799e8ad13
Justin Su <injustsu@gmail.com>
2026-02-18 07:53:37 -0500

    [refactor] Use `and` instead of `&&`

--- tests/configure_bindings/key_sequences.fish
+++ tests/configure_bindings/key_sequences.fish
@@ -15,7 +15,7 @@ fzf_configure_bindings --directory=ctrl-a --git-log=ctrl-b --git-status=ctrl-c -
 @test "can override the default binding for history" (binding_contains_func ctrl-d _fzf_search_history) $status -eq 0
 @test "can override the default binding for variables" (binding_contains_func ctrl-e $_fzf_search_vars_command) $status -eq 0
 
-bind --mode=insert ctrl-a >/dev/null && bind --mode=insert ctrl-e >/dev/null
+bind --mode=insert ctrl-a >/dev/null; and bind --mode=insert ctrl-e >/dev/null
 @test "installs bindings for insert mode" $status -eq 0
 
 _fzf_uninstall_bindings
@@ -24,7 +24,7 @@ _fzf_uninstall_bindings
 # intentionally test both style of passing options with no value
 fzf_configure_bindings --directory --git-status=
 @test "can erase bindings by passing no key sequence" -z (bind --user | string match -r --entire '_fzf_search_directory|_fzf_search_git_status')
-binding_contains_func ctrl-alt-l _fzf_search_git_log && binding_contains_func ctrl-r _fzf_search_history && binding_contains_func ctrl-v $_fzf_search_vars_command
+binding_contains_func ctrl-alt-l _fzf_search_git_log; and binding_contains_func ctrl-r _fzf_search_history; and binding_contains_func ctrl-v $_fzf_search_vars_command
 @test "installs default bindings that aren't customized" $status -eq 0
 
 fzf_configure_bindings --unknown=ctrl-q &>/dev/null
