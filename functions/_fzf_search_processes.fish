1f81c99e16cb6a6afcd85c2057d749276582ed10
Justin Su <injustsu@gmail.com>
2026-02-18 07:54:56 -0500

    [refactor] Use `or` instead of `||`

--- functions/_fzf_search_processes.fish
+++ functions/_fzf_search_processes.fish
@@ -1,7 +1,7 @@
 function _fzf_search_processes --description="Search all running processes. Replace the current token with the pid of the selected process."
     # Directly use ps command because it is often aliased to a different command entirely
     # or with options that dirty the search results and preview output
-    set -f ps_cmd (command -v ps || echo "ps")
+    set -f ps_cmd (command -v ps; or echo "ps")
     # use all caps to be consistent with ps default format
     # snake_case because ps doesn't seem to allow spaces in the field names
     set -f ps_preview_fmt (string join ',' 'pid' 'ppid=PARENT' 'user' '%cpu' 'rss=RSS_IN_KB' 'start=START_TIME' 'command')
@@ -14,7 +14,7 @@ function _fzf_search_processes --description="Search all running processes. Repl
                     # first line outputted by ps is a header, so we need to mark it as so
                     --header-lines=1 \
                     # ps uses exit code 1 if the process was not found, in which case show an message explaining so
-                    --preview="$ps_cmd -o '$ps_preview_fmt' -p {1} || echo 'Cannot preview {1} because it exited.'" \
+                    --preview="$ps_cmd -o '$ps_preview_fmt' -p {1}; or echo 'Cannot preview {1} because it exited.'" \
                     --preview-window="bottom:4:wrap" \
                     $fzf_processes_opts
     )
