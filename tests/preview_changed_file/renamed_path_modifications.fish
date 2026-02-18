f4fe1a3b3af436cb8c93d35617f1822497022897
Justin Su <injustsu@gmail.com>
2026-02-18 07:53:37 -0500

    [refactor] Use `and` instead of `&&`

--- tests/preview_changed_file/renamed_path_modifications.fish
+++ tests/preview_changed_file/renamed_path_modifications.fish
@@ -9,8 +9,7 @@ set output (_fzf_preview_changed_file "R  \"$orig_path\" -> \"$renamed_path\"")
 
 # test that the added line shows up in the diff but not the entire file
 # "aaaaaaaaaa" is the first line in the file
-string match -q --entire $added_line $output &&
-    not string match --entire aaaaaaaaaa $output
+string match -q --entire $added_line $output; and not string match --entire aaaaaaaaaa $output
 @test "shows only the modifications made to renamed file" $status -eq 0
 
 git mv $renamed_path $orig_path
