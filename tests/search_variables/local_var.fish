8d3c6856e9445024c91febf67be3f6e8302ffa9e
Justin Su <injustsu@gmail.com>
2026-04-06 23:39:26 -0400

    [refactor] Normalize usage of `commandline --current-token --replace`

--- tests/search_variables/local_var.fish
+++ tests/search_variables/local_var.fish
@@ -1,6 +1,6 @@
 set -l a_local_variable
 set -xa FZF_DEFAULT_OPTS "--filter=a_local_variable"
-mock commandline "--current-token --replace" "echo \$argv" # instead of updating commandline with the result, just output it
+mock commandline "--current-token --replace --" "echo \$argv" # instead of updating commandline with the result, just output it
 mock commandline \* "" # mock all other commandline executions to do nothing
 set actual (eval $_fzf_search_vars_command)
 @test "can find a local variable" "$actual" = a_local_variable
