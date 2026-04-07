ac3289bf9e4ad4f3ed593c0befdb5da6bc01d580
Justin Su <injustsu@gmail.com>
2026-04-06 23:39:26 -0400

    [refactor] Normalize usage of `commandline --current-token --replace`

--- tests/search_git_log/outputs_right_commit.fish
+++ tests/search_git_log/outputs_right_commit.fish
@@ -1,5 +1,5 @@
 # This test has two versions: one for CI and one for local
-mock commandline "--current-token --replace" "echo \$argv"
+mock commandline "--current-token --replace --" "echo \$argv"
 mock commandline \* ""
 if git cat-file -e c6326dbda6b1f48ecbd015838073213be3bf6ec1 2>/dev/null # sha is a random commit that CI wouldn't pull
     # This test is running locally.
